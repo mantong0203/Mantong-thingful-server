fb4c6652f0fb05bf1fc500fbd726e1a439cb1e72
Mantong Zhao <zhaomantong0203@gmail.com>
2019-07-30 13:26:05 -0700

    comparing hased password

--- seeds/seed.thingful_tables.sql
+++ seeds/seed.thingful_tables.sql
@@ -8,12 +8,12 @@ TRUNCATE
 
 INSERT INTO thingful_users (user_name, full_name, nickname, password)
 VALUES
-  ('dunder', 'Dunder Mifflin', null, 'password'),
-  ('b.deboop', 'Bodeep Deboop', 'Bo', 'bo-password'),
-  ('c.bloggs', 'Charlie Bloggs', 'Charlie', 'charlie-password'),
-  ('s.smith', 'Sam Smith', 'Sam', 'sam-password'),
-  ('lexlor', 'Alex Taylor', 'Lex', 'lex-password'),
-  ('wippy', 'Ping Won In', 'Ping', 'ping-password');
+  ('dunder', 'Dunder Mifflin', null, '$2a$12$GaKHT5VhNZOYmLqGoroUGuXnXM6vVRCQMUJZJ/nfXiPPHUTAubE7m'),
+  ('b.deboop', 'Bodeep Deboop', 'Bo', '$2a$12$V1HghkvmXObxOlJCeZ0S.u8FQg2wv9eOY7OCbrsEXy.C0TKxubB1a'),
+  ('c.bloggs', 'Charlie Bloggs', 'Charlie', '$2a$12$xgBR6CiJ.U0N4McgLUKBdu8Sc68qjQTXgw3K6g8EWX/SMZqKEr2Vy'),
+  ('s.smith', 'Sam Smith', 'Sam', '$2a$12$jzMGf1FZhRKnVrlslYHhhOra/vbwPg0zVfzbK7t2FqpbL7XlLw0GG'),
+  ('lexlor', 'Alex Taylor', 'Lex', '$2a$12$6V9AInIJwmTvf4xzrI1IB.yxG3jrRsu1lvNwBpQdg4y6nxbW1Wl2.'),
+  ('wippy', 'Ping Won In', 'Ping', '$2a$12$NYLifM6rHwpTfr1DnAM4cOQs9gWeUf24BhFtTiggnGgWR2JcLy0KG');
 
 INSERT INTO thingful_things (title, image, user_id, content)
 VALUES
